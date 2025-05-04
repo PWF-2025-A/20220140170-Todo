4bec3b5022f4bd32468c2e5def3f873bd8d9f424
kaniadhea <127089007+kaniadhea@users.noreply.github.com>
2025-05-04 23:53:42 +0700

    middleware, gate admin

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,33 @@
 <?php
- 
- use App\Http\Controllers\ProfileController;
- use App\Http\Controllers\TodoController;
- use App\Http\Controllers\UserController;
- use Illuminate\Support\Facades\Route;
- 
- Route::get('/', function () {
-     return view('welcome');
-    });
- 
- Route::get('/dashboard', function () {
-     return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
- 
- Route::middleware('auth')->group(function () {
-     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
- 
-     Route::get('/todo', [TodoController::class, 'index'])->name('todo.index');
-     Route::get('/todo/create', [TodoController::class, 'create'])->name('todo.create');
 
- 
-     Route::get('/user', [UserController::class, 'index'])->name('user.index');
-     
-     Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
-     Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
-     Route::delete('/todo/{todo}', [TodoController::class, 'destroy'])->name('todo.destroy');
-     Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Route;
 
-     
-     Route::resource('todo', TodoController::class)->except(['show']);
- 
-   
-     Route::get('/user', [UserController::class, 'index'])->name('user.index');
-     Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
-     Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
-     Route::delete('/user/{user}', [UserController::class, 'destroy'])->name('user.destroy');
-    });
- 
- require __DIR__.'/auth.php';
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('/todo', TodoController::class)->except(['show']);
+    Route::delete('/todo', [TodoController::class, 'destroyCompleted'])->name('todo.deleteallcompleted');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+});
+
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::resource('/user', UserController::class)->except(['show']);
+    Route::patch('/user/{user}/makeadmin', [UserController::class, 'makeadmin'])->name('user.makeadmin');
+    Route::patch('/user/{user}/removeadmin', [UserController::class, 'removeadmin'])->name('user.removeadmin');
+});
+
+require __DIR__.'/auth.php';
---
+++ tugas7.md
@@ -0,0 +1,8 @@
+# Tugas 7
+
+
+# 1.  Middleware & Gate Admin Not Active 
+![alt text](screenshot/tugas7/Search.png)
+
+# 2. Middleware & Gate Admin Active
+![alt text](screenshot/tugas7/Active.png)
